c2db3450bba409e46d8334000dce05edca1e8802
Martyna Stepien <martyna.stepien@ldntranml010706.local>
2017-06-09 17:28:27 +0100

    converter finished up to X symbol

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,14 @@
 function convert(arabic){
-  roman = "";
-  while (arabic >= 10) {
-    roman = "X";
-    arabic = arabic - 10;
-  }
-  while (arabic >= 5) {
-    roman = "V";
-    arabic = arabic - 5;
-  }
-  while (arabic === 4) {
-    roman = "IV";
-    arabic = arabic - 4;
-  }
-  while (arabic >= 1) {
-    roman += "I";
-    arabic --;
-  }
-  return roman;
-}
+
+  var decimals = [10, 9, 5, 4, 1];
+  var symbols = ['X', 'IX', 'V', 'IV', 'I'];
+  var roman = "";
+
+for (var i = 0; i< symbols.length; i++) {
+      	while (arabic>= decimals[i]) {
+      		roman += symbols[i];
+      		arabic -= decimals[i];
+      	}
+      }
+	return roman;
+};
